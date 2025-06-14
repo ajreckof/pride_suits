83cda847a99957a9c08c35682eeb6574621dd4d4
ajreckof <tbonhoure@ymail.Com>
2025-06-14 12:48:52 +0200

    change to low contrast because they clearly are

--- main.lua
+++ main.lua
@@ -2,27 +2,30 @@ local ranks = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', "K
 local display_ranks = {"Ace","King", "Queen", "Jack"}
 
 
+
 local lgbt_deck = SMODS.Atlas {
 	key = "lgbt_deck",
 	path = "LGBT_Deck.png",
 	px = 71,
 	py = 95,
 }
+
+
 SMODS.DeckSkin {
 	key = "bisexual",
 	suit = "Hearts",
 	loc_txt = "Bisexual pride",
 	palettes = {
 		{
-			key = 'hc',
+			key = 'lc',
 			ranks = ranks,
 			display_ranks = display_ranks,
 			atlas = lgbt_deck.key,
 			pos_style = 'deck',
-			colour = HEX("9734f0"),
 		},
 	},
 }
+
 
 SMODS.DeckSkin {
 	key = "lesbian",
@@ -30,15 +33,15 @@ SMODS.DeckSkin {
 	loc_txt = "Lesbian pride",
 	palettes = {
 		{
-			key = 'hc',
+			key = 'lc',
 			ranks = ranks,
 			display_ranks = display_ranks,
 			atlas = lgbt_deck.key,
 			pos_style = 'deck',
-			colour = HEX("9734f0"),
 		},
 	},
 }
+
 
 SMODS.DeckSkin {
 	key = "pansexual",
@@ -46,15 +49,15 @@ SMODS.DeckSkin {
 	loc_txt = "Pansexual pride",
 	palettes = {
 		{
-			key = 'hc',
+			key = 'lc',
 			ranks = ranks,
 			display_ranks = display_ranks,
 			atlas = lgbt_deck.key,
 			pos_style = 'deck',
-			colour = HEX("9734f0"),
 		},
 	},
 }
+
 
 SMODS.DeckSkin {
 	key = "gay_men",
@@ -62,12 +65,11 @@ SMODS.DeckSkin {
 	loc_txt = "Gay pride",
 	palettes = {
 		{
-			key = 'hc',
+			key = 'lc',
 			ranks = ranks,
 			display_ranks = display_ranks,
 			atlas = lgbt_deck.key,
 			pos_style = 'deck',
-			colour = HEX("9734f0"),
 		},
 	},
 }
